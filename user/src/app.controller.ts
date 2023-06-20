867784cd7741cbc47d2f8be8a7284b56c94847bf
dennishooo <hkhdennis@gmail.com>
2023-06-21 07:36:46 +0800

    user: update controller

--- user/src/app.controller.ts
+++ user/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientKafka, EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject('USER_SERVICE') private readonly userClient: ClientKafka,
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('user_created')
+  handleUserCreated(data: any) {
+    this.appService.handleUserCreated(data.value);
   }
 }
--- user/src/app.service.ts
+++ user/src/app.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('USER_SERVICE') private readonly userClient: ClientKafka,
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }
