fb9baae2fce4decc1264b0c352db40257c84c7e9
dennishooo <hkhdennis@gmail.com>
2023-06-21 07:12:46 +0800

    api-gateway: add kafka client in service

--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('USER_SERVICE') private readonly userClient: ClientKafka,
+  ) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createUser({}: CreateUserRequest) {
+    this.userClient.emit('user_created', new UserCreatedEvent('dennis', 'ho'));
   }
 }
